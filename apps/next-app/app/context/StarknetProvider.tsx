89659135b457d9b4dac6556337abad1832c6fba7
Asten <0xasten@gmail.com>
2024-11-27 20:30:37 +0800

    add prove tokens session

--- apps/next-app/app/context/StarknetProvider.tsx
+++ apps/next-app/app/context/StarknetProvider.tsx
@@ -20,6 +20,15 @@ const STRK_TOKEN_ADDRESS =
 const SWAP_USDC_STRK_ADDRESS =
   '0x4270219d365d6b017231b52e92b3fb5d7c8378b05e9abc97724537a80e93b0f'
 
+const DAI_TOKEN_ADDRESS =
+  '0xda114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3'
+
+const USDT_TOKEN_ADDRESS =
+  '0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
+
+const LORDS_TOKEN_ADDRESS =
+  '0x124aeb495b947201f5fac96fd1138e326ad86195b98df6dec9009158a533b49'
+
 const connector = new ControllerConnector({
   policies: [
     {
@@ -36,6 +45,21 @@ const connector = new ControllerConnector({
       target: STRK_TOKEN_ADDRESS,
       method: 'approve',
       description: 'Approve STRK token for the swap.',
+    },
+    {
+      target: DAI_TOKEN_ADDRESS,
+      method: 'approve',
+      description: 'Approve DAI token for the swap.',
+    },
+    {
+      target: USDT_TOKEN_ADDRESS,
+      method: 'approve',
+      description: 'Approve USDT token for the swap.',
+    },
+    {
+      target: LORDS_TOKEN_ADDRESS,
+      method: 'approve',
+      description: 'Approve LORDS token for the swap.',
     },
     {
       target: SWAP_USDC_STRK_ADDRESS,
